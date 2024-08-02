89668127c02b12f13bcfd0c6cce777150c93ab56
Caleb <clbayles@hotmail.com>
2024-08-02 15:06:34 -0500

    Revert "attempt at a more efficient SQL query, but the result won't stay in order :("
    This reverts commit 138aad8e7d862a5ee4bc21c6c33926b484f158ec.

--- Models/GreebloDealer.cs
+++ Models/GreebloDealer.cs
@@ -4,29 +4,23 @@ namespace backend.Models;
 
 public class GreebloDealer(DatabaseCredentials credentials) : GenericDealer(credentials) {
 
-    public async Task<float[]> GetStatus(string schedule) {
-        ChocolateBarDealer cacaoDealer = new(credentials);
+    public async Task<float[]> GetStatus(int[] schedule) {
         Greeblo greeblo = new() {
             Health = 5,
             Cacao = 75,
-            Schedule = await cacaoDealer.GetMultiple(schedule)
+            Schedule = await TranslateSchedule(schedule)
         };
-        for (int i = 0; i < greeblo.Schedule.Length; i++) {
-            if (greeblo.Schedule[i] == null) {
-                Console.WriteLine($"{i}: null");
-            } else {
-                Console.WriteLine($"{i}: {greeblo.Schedule[i].Id}");
-            }
-        }
+        Console.WriteLine(greeblo.Schedule[2].CacaoPercent);
         return [];
+
     }
 
-    // private async Task<ChocolateBar[]> TranslateSchedule(int[] schedule) {
-    //     ChocolateBar[] result = new ChocolateBar[21];
-    //     ChocolateBarDealer cacaoDealer = new(credentials);
-    //     for (int i = 0; i < schedule.Length; i++) {
-    //         result[i] = await cacaoDealer.Get(schedule[i]);
-    //     }
-    //     return result;
-    // }
+    private async Task<ChocolateBar[]> TranslateSchedule(int[] schedule) {
+        ChocolateBar[] result = new ChocolateBar[21];
+        ChocolateBarDealer cacaoDealer = new(credentials);
+        for (int i = 0; i < schedule.Length; i++) {
+            result[i] = await cacaoDealer.Get(schedule[i]);
+        }
+        return result;
+    }
 }
